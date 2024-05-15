f7940021de4b042bd001d002511722045dcf96fb
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-15 16:55:51 +0200

    Mise a niveau de Testssss

--- Question2.php
+++ Question2.php
@@ -6,8 +6,44 @@
     <title>Question 2</title>
 </head>
 <body>
+    <pre>
 <?php
-echo "coucou";
+print_r($_SERVER);
+
+echo "<table border='2px'>
+<thead>
+    <tr>
+        <th>Clé</th>
+        <th>Valeur</th>
+    </tr>
+</thead>
+
+<tbody>";
+echo " <tr>
+<td>HTTP_HOST</td>
+<td>{$_SERVER['HTTP_HOST']}</td>
+</tr>";
+echo $_SERVER["HTTP_HOST"];
+
+echo "</tbody>
+</table>";
+
 ?>
+
+<table border="2px">
+    <thead>
+        <tr>
+            <th>Col1</th>
+            <th>Col2</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>Valeur 1</td>
+            <td>Valeur 2</td>
+        </tr>
+    </tbody>
+</table>
 </body>
 </html>
--- Testsssssss.php
+++ Testsssssss.php
@@ -43,5 +43,47 @@ for ($i = 1; ;$i++){
 
 
 ?>
+
+
+<!-- Question 2 -->
+<pre>
+<?php
+print_r($_SERVER);
+
+echo "<table border='2px'>
+<thead>
+    <tr>
+        <th>Clé</th>
+        <th>Valeur</th>
+    </tr>
+</thead>
+
+<tbody>";
+echo " <tr>
+<td>HTTP_HOST</td>
+<td>{$_SERVER['HTTP_HOST']}</td>
+</tr>";
+echo $_SERVER["HTTP_HOST"];
+
+echo "</tbody>
+</table>";
+
+?>
+
+<table border="2px">
+    <thead>
+        <tr>
+            <th>Col1</th>
+            <th>Col2</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>Valeur 1</td>
+            <td>Valeur 2</td>
+        </tr>
+    </tbody>
+</table>
 </body>
 </html>
