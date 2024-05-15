4435018c128d08cb651600bc604623dd4d2c06f6
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-15 14:23:50 +0200

    Question 1 fini

--- Question1.php
+++ Question1.php
@@ -2,5 +2,46 @@
 utilisé fréquemment pour présenter les techniques de programmation de chaque langage. -->
 
 <?php
+echo "Hello World";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 1</title>
+</head>
+<body>
+<?php
+echo "Hello World <br>";
+
+/* exemple 1 */
+for ($i = 1; $i <= 10; $i++){
+    echo $i;
+}
+
+$mot = "Hello World <br>";
+for ($i = 1; $i <= 10; $i++){
+    echo $mot;
+}
+
+for ($i = 1; $i <= 10; $i++){
+    echo "<font size ='".$i."'> Hello World !!!!! </font><br>";
+}
+
+
+
+
+
+/* exemple 2 */
+for ($i = 1; ;$i++){
+    if ($i > 10) {
+        break;
+    }
+    echo $i;
+}
+
 
 ?>
+</body>
+</html>
