c4b6c665fda8c4dc7385da5976c74e318ea60f8b
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-15 14:03:59 +0200

    Début des exo sur métis PHP

---
+++ Question1.php
@@ -0,0 +1,6 @@
+<!-- Ce premier exercice est une variante du programme d'affichage du message «Hello World!» 
+utilisé fréquemment pour présenter les techniques de programmation de chaque langage. -->
+
+<?php
+
+?>
