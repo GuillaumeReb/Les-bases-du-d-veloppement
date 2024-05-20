b957fa411d5673cc4f687c8a5fd65141b6f8b449
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-20 13:54:50 +0200

    Mise en place de 02Tableau

--- 20_Enonces_3_exos-supp_tableaux/mult_html_v2_V2.php
+++ 20_Enonces_3_exos-supp_tableaux/mult_html_v2_V2.php
@@ -1,9 +1,45 @@
 <?php
+
+// Le debut de la page HTML
+$page = "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Table de multiplication Version 3 (Maximum PHP et 1 seul ECHO)</title>
+    <style>
+        body {
+            background-color: black;
+        }
+        td, th {
+            width: 100px;
+            text-align: center;
+        }
+        .titre {
+            background-color: black;
+            color: magenta;
+        }
+        .tableau-gris {
+            background-color: lightgrey;
+            color: white;          
+        }
+        .tableau-noir {
+            background-color: black;
+            color: lightgrey;          
+        }
+    </style>
+</head>
+<body>";
+
+
+
+
 $titre = "<h1>Une table de multiplication</h1>";
 echo $titre;
 
-$table = "<table>";
-$head = "<thead>
+$table = "<table>
+<thead>
 <tr>
     <th>1</th>
     <th>2</th>
@@ -17,132 +53,19 @@ $head = "<thead>
     <th>10</th>
 </tr>
 </thead>";
-$body = "<tbody>
+$table .= '<tbody>';
 for ($i=2; $i <= 10; $i++) { 
-<tr>
-    <td>$i</td>
+    $table .= "<tr>";
+    $table .= "<td>$i</td>";
     for ($j=2; $j<=10; $j++){
-        <td>.$i*$j.</td>   
+        $table .= "<td>$i*$j</td>";   
         }    
-        </tr>
+        $table .= "</tr>";
     }
-</tbody>";
-$table .= "</table>";
+
+$table .= "</tbody>  </table>  </body>
+</html>";
 
 echo $table;
-echo $head;
-echo $body;
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Petit 2</title>
-    <style>
-        /* body{
-            background-color : black;
-        } */
-    </style>
-</head>
-<body>
-
-<h1>Une table de multiplication</h1>
-
-    <table>
-        <thead>
-            <tr>
-                <th>1</th>
-                <th>2</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-                <th>7</th>
-                <th>8</th>
-                <th>9</th>
-                <th>10</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>2</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) { 
-                    $resu = $i*2;
-                    echo "<td>$resu</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>3</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) { 
-                    $resu = $i*3;
-                    echo "<td>$resu</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>4</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) { 
-                    $resu = $i*4;
-                    echo "<td>$resu</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>5</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) {                     
-                    echo "<td>".($i*5)."</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>6</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) {                     
-                    echo "<td>".($i*6)."</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>7</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) {                     
-                    echo "<td>".($i*7)."</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>8</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) {                     
-                    echo "<td>".($i*8)."</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>9</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) {                     
-                    echo "<td>".($i*9)."</td> ";
-                }
-                ?>
-            </tr>
-            <tr>
-                <td>10</td>
-                <?php
-                for ($i=2; $i <= 10; $i++) {                     
-                    echo "<td>".($i*10)."</td> ";
-                }
-                ?>
-            </tr>
-        </tbody>
-    </table>
-    
-</body>
-</html>
