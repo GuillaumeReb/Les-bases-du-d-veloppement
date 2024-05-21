3c39040f40ea8b08e361abf941f98bc224d8c9ae
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-21 18:14:38 +0200

    ajout de form1.css

--- 30_Enonces_2_exos_suppl_Date_redirect_DEPOT/form1.php
+++ 30_Enonces_2_exos_suppl_Date_redirect_DEPOT/form1.php
@@ -57,36 +57,26 @@ if (isset($_POST["btn"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form1</title>
-    <style>
-        .titre{
-            text-align:center;
-        }
-        .form{
-            text-align:center;
-        }
-
-    </style>
+    <link rel="stylesheet" href="form1.css">
 </head>
 <body>
-    <br>
+    <div class="container">
+   
 <h1 class="titre">Page de connection</h1>
-<br>
-<br>
-<?php
-echo $msg;
-?>
-<br>
-<br>
 <form action="form1.php" method="POST" class="form">
     <label for="txtnom">Entrez votre email </label>
-    <input type="mail" id="txtmail" name="mail"><br>
-    <br>
+    <input type="mail" id="txtmail" name="mail" class="input"><br>
+  
     <label for="txtpwd">Entrez votre mot de passe</label>
-    <input type="password" id="txtpwd" name="pwd"><br>
-    <br>
-    <input type="submit" value="Envoyer" name="btn">
+    <input type="password" id="txtpwd" name="pwd" class="input"><br>
+    
+    <input type="submit" value="Envoyer" name="btn" class="submit">
     <!-- <input type="reset" value="Recommencer"> -->
 </form>
-    
+<?php
+echo "<p class='mess'>$msg</p>";
+?>
+</div>
+
 </body>
 </html>
