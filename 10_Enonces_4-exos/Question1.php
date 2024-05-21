290e557b898bb3079411959a469e83172530fec2
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-21 11:00:14 +0200

    Mise en place de la page Header

--- 10_Enonces_4-exos/Question1.php
+++ 10_Enonces_4-exos/Question1.php
@@ -1,3 +1,7 @@
+<?php
+include "../Header.php";
+?>
+
 <!-- Ce premier exercice est une variante du programme d'affichage du message «Hello World!» 
 utilisé fréquemment pour présenter les techniques de programmation de chaque langage. -->
 
