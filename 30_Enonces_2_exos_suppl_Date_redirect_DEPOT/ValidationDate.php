8fd913beff38a96bd6f2fbcb3fa52b07ac74cad3
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-05-22 10:10:12 +0200

    ValidationDate et son css OK

--- 30_Enonces_2_exos_suppl_Date_redirect_DEPOT/ValidationDate.php
+++ 30_Enonces_2_exos_suppl_Date_redirect_DEPOT/ValidationDate.php
@@ -2,7 +2,7 @@
 include "../Header.php";
 function verifier($date, $format = 'd-m-Y H:i:s')
 {   
-    $date = $_GET['date'];
+    // $date = $_GET['date'];
     $d = DateTime::createFromFormat($format, $date);
     return $d && $d->format($format) == $date;
 }
@@ -13,17 +13,17 @@ function verifier($date, $format = 'd-m-Y H:i:s')
 
 if (isset($_GET['submit'])) {
     $date = $_GET['date'];
-    echo "<br><h3 class='titre'>La date saisi est le " .$date."</h3><br>";
-}
+    $mesg1 = "<br><h3 class='titre'>La date saisi est le " .$date."</h3><br>";
+    }
 
 // var_dump(verifier($date, 'd/m/Y'));
 
-if (verifier($date, 'd/m/Y') === true) {
-    $mess = "<br><p class='mess'>La date saisie existe</p></br>";
-} else {
-    $mess = "<br><p class='mess2'>La date saisie est fausse</p></br>";
-}
 
+if (verifier($date, 'd/m/Y') === true) {
+    $mess = "<br><p class='mess'>La date saisie existe</p><br>";
+} else {
+    $mess = "<br><p class='mess2'>La date saisie est fausse</p><br>";
+} 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,43 +31,23 @@ if (verifier($date, 'd/m/Y') === true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VerifDate</title>
-    <style>
-        .titre{
-            text-align:center;
-        }
-        .form{
-            text-align:center;
-        }
-        h4{
-            text-align:center;
-        }
-        .mess{
-            text-align:center;
-            font-size:1.2rem;
-            font-weight: bold;
-            color:green;
-
-        }
-        .mess2{
-            text-align:center;
-            font-size:1.2rem;
-            font-weight: bold;
-            color:red;
-
-        }
-    </style>
+    <link rel="stylesheet" href="ValidationDate.css">
 </head>
 <body>
-    <h4>Entrez une date au forma jj/mm/yyyy </h4>
+    <br>
+    <br>
+<div class="form-container">
+    <h4>Entrez une date au forma jj/mm/yyyy </h4>    
     <form action="./ValidationDate.php" class="form">
         <input type="text" name="date" placeholder="20/05/2024" >
         <button type="submit" name="submit">Test</button>
     </form>
     <br>
     <?php
+    echo $mesg1;
     // echo "<br>La date saisie est " .($date)."<br>";
     echo $mess;
     ?>
-
+ </div>
 </body>
 </html>
